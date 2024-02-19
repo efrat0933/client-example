9822a9227eba0135bf2cb6b6607151f2222e98f5
efratdev0933 <efratdev0933@gmail.com>
2024-02-19 17:39:53 +0200

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: []
 })
 export class AppComponent {
   title = 'client-example';
+
+  constructor(private http: HttpClient) {
+    
+  }
+
+  getAllUsers() {
+    this.http.get('http://localhost:3000/api/users').subscribe(res => {
+      console.log(res);
+    })
+  }
 }
